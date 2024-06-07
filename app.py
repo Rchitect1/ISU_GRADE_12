b8893a397f9eb19b3458cd93d4da6ae7ce894535
KSFn1 <103542648+KSFn1@users.noreply.github.com>
2024-06-07 21:43:58 +0000

    login done

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ def matches():
 @app.route("/login")
 def login():
     return render_template("login.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 @app.route("/student_form")
 def student_form():
