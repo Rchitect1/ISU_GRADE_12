6cd84284e5ad777429ebe1282d63d3f2f23651a9
Reza Homayounmehr <87591586+Rchitect1@users.noreply.github.com>
2024-06-08 18:03:56 +0000

    second page

--- app.py
+++ app.py
@@ -17,17 +17,22 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/matches")
 def matches():
-    return render_template("matches.html")
+    con = sqlite3.connect("peer_tutor.db")
+    cur = con.cursor()
+    res = cur.execute("select students.name, students.grade, tutors.name, tutors.grade, students.subject from students, tutors where students.id = tutors.match_id;")
+    return render_template("matches.html", matches_list=res.fetchall())
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
