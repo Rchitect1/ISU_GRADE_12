10df00806e2c715e1dbfb26cad2f8f3ac242ddb0
KSFn1 <103542648+KSFn1@users.noreply.github.com>
2024-06-08 14:30:43 +0000

    login done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_session import Session
 import sqlite3
+from bs4 import BeautifulSoup
 
 conn = sqlite3.connect("peer_tutor.db")
 c = conn.cursor
@@ -33,6 +34,23 @@ def student_form():
 @app.route("/tutor_form")
 def tutor_form():
     return render_template("tutor_form.html")
+
+
+@app.route("/submit_login", methods=["POST"])
+def submit_login():
+    email = request.form["email"]
+    password = request.form["password"]
+    code = request.form["code"]
+
+    if code == "123456":
+        return redirect(url_for("matches"))
+    else:
+        return redirect(url_for("login", showModal='true', message="Invalid code"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 # def doMatchmaking():
 #     return null
