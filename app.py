3d6aa0a88eb9463bf539700cd090bede37bcada2
KSFn1 <103542648+KSFn1@users.noreply.github.com>
2024-06-13 00:25:37 +0000

    small update to mm

--- app.py
+++ app.py
@@ -261,19 +261,25 @@ def matchAvailable(student, tutor): #takes 2 arrays
         #pair up student and tutor (if eligable)
             if (student[9] == tutor[9]):
                 if ((student[3][0] == 'S' and tutor[3][0] == 'S') or (student[3][0] == 'C' and tutor[3][0] == 'C')): #checks first letter (subject))
-                    if (student[3][1] == tutor[3][1]):
+
+                    if(student[3][0] == 'S' and tutor[3][0] == 'S'):
+                        if (int (student [3][-2]) <= 2 and int (tutor[3][-2]) >=2 ):
+                            c.execute("UPDATE students SET match = ? WHERE id = ?;", (tutor[0], student[0]))
+                            c.execute("UPDATE tutors SET match = ? WHERE id = ?;", (student[0], tutor[0]))
+
+                    elif (student[3][1] == tutor[3][1]):
                         c.execute("UPDATE students SET match = ? WHERE id = ?;", (tutor[0], student[0]))
                         c.execute("UPDATE tutors SET match = ? WHERE id = ?;", (student[0], tutor[0]))
 
                 elif (student[3][0] == tutor[3][0]): #checks first letter (subject)
                     if (int(student[3][-2]) < int (tutor[3][-2])): #checks second last letter (grade)
-                            c.execute("UPDATE students SET match = ? WHERE id = ?;", (tutor[0], student[0]))
-                            c.execute("UPDATE tutors SET match = ? WHERE id = ?;", (student[0], tutor[0]))
+                        c.execute("UPDATE students SET match = ? WHERE id = ?;", (tutor[0], student[0]))
+                        c.execute("UPDATE tutors SET match = ? WHERE id = ?;", (student[0], tutor[0]))
 
-                    elif(int(student[3][-2]) == int (tutor[3][-2])): #checks second last latter (grade)
-                        if ((student[3][-1]) <= tutor[3][-1]): #checks last letter (c/U)
-                            c.execute("UPDATE students SET match = ? WHERE id = ?;", (tutor[0], student[0]))
-                            c.execute("UPDATE tutors SET match = ? WHERE id = ?;", (student[0], tutor[0]))          
+                elif(int(student[3][-2]) == int (tutor[3][-2])): #checks second last latter (grade)
+                    if ((student[3][-1]) <= tutor[3][-1]): #checks last letter (c/U)
+                        c.execute("UPDATE students SET match = ? WHERE id = ?;", (tutor[0], student[0]))
+                        c.execute("UPDATE tutors SET match = ? WHERE id = ?;", (student[0], tutor[0]))          
                 
     conn.commit()
     conn.close()
